756efbe8d4cccefd0be78d22cb6c7bb2df6199b6
Aaron Mahoney <aaronmahoney1993@outlook.com>
2025-02-17 14:03:35 -0700

    created datafiles, routes, and added dependencies

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
-
 const PORT = process.env.PORT
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the server")
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
