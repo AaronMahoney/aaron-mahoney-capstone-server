691bba0f3687694d20acf932e109910958e90e3f
Aaron Mahoney <aaronmahoney1993@outlook.com>
2025-02-18 18:17:23 -0700

    created json shells, updated index.js, and created the start of various routes

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ import cors from "cors";
 import "dotenv/config";
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to the server")
-})
+});
+
+app.use("/people", people);
+app.use("/places", places);
+app.use("/sayings", sayings);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
